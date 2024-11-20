dd7cd1b395969634ec54a6d53b791e2852fb22a2
Estasleyendoesto <alsival@hotmail.com>
2024-11-20 18:51:20 +0100

    corregido

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,5 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://paginasestaticas.github.io/',
-	integrations: [mdx(), sitemap({
-		filter: (page) =>
-		  page !== 'https://stargazers.club/secret-vip-lounge-1/' &&
-		  page !== 'https://stargazers.club/secret-vip-lounge-2/' &&
-		  page !== 'https://stargazers.club/secret-vip-lounge-3/' &&
-		  page !== 'https://stargazers.club/secret-vip-lounge-4/',
-	  })],
+	integrations: [mdx(), sitemap()],
 });
