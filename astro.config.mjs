d67154b5055d3c8bde23260f0188a391a60d6248
Estasleyendoesto <alsival@hotmail.com>
2024-11-20 19:12:31 +0100

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-	site: 'https://paginasestaticas.github.io/BJFontaneros/',
+	site: 'https://paginasestaticas.github.io',
+  	base: 'BJFontaneros',
 	integrations: [mdx(), sitemap()],
 });
