76d55a3c96cacbd64853bafc85b0ddcfd4f134fe
EEsto <alsival@hotmail.com>
2025-08-08 10:23:16 +0200

    cambio logotipo, tarjetas, y enlace instagram, nueva build

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Fontaneros';
-export const SITE_DESCRIPTION = 'Fontanería y Reforma Profesional en Madrid';
+export const SITE_TITLE = 'Reformas Integrales B & J | Profesionales en Madrid';
+export const SITE_DESCRIPTION = 'Expertos en reformas de viviendas, cocinas, baños y locales en Madrid con resultados profesionales y garantizados.';
